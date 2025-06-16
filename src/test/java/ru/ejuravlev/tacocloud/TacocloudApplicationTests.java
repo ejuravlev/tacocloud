0903e61bf05d809f849c38d726e8143118441269
ejuravlev <e.juravlev@skbkontur.ru>
2025-06-16 12:40:43 +0500

    init

---
+++ src/test/java/ru/ejuravlev/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ejuravlev.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
