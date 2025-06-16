45eb106bd18fe1fe589386fdc096565cf666f4d3
ejuravlev <e.juravlev@skbkontur.ru>
2025-06-16 21:26:36 +0500

    Replace HomeController
    Replace it as ViewController

--- src/main/java/ru/ejuravlev/tacocloud/TacocloudApplication.java
+++ src/main/java/ru/ejuravlev/tacocloud/TacocloudApplication.java
@@ -2,12 +2,20 @@ package ru.ejuravlev.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacocloudApplication {
+public class TacocloudApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacocloudApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(@NonNull ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
 	}
 
 }
